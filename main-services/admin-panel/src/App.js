01a1a23f961b78b9c4067c539fc3955b1b0ab190
Alexandr Dmowski <dmowski@yandex.ru>
2021-11-15 02:45:10 +0300

    REmove unnecessary files

--- main-services/admin-panel/src/App.js
+++ main-services/admin-panel/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Welcome from "./Welcome";
 import Secured from "./Secured";
-import "./App.css";
-
 class App extends Component {
   render() {
     return (
