97475012a39a4c089c5840ff27706a7900df7401
Alexandr Dmowski <dmowski@yandex.ru>
2021-11-14 21:21:26 +0300

    Update readme/configs for realm

--- profolio-servises/admin-panel-api/config/keycloak-config.js
+++ profolio-servises/admin-panel-api/config/keycloak-config.js
@@ -5,7 +5,7 @@ let _keycloak;
 var keycloakConfig = {
   realm: "MyDemo",
   "realm-public-key":
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjYdEtqbUOqLloB2Y6ghEnme5RYUNwcDxh6tPiF3kaMA3ew+GOdHwWnK0vcbXxK87GH5K62LN2uC5B+15uknblynjvaTRApCjnKypr+3BH8clbktOrpzB46oR816hL5c7uBIITT4z8TEkUmdEL4/QojZqAsemcn8VnXmQ1uZBBdfhVy+KvCJgN6FR+nPstkLjYZGfB9AdAY4Jy8p2Cb6pnboOAqeAdSkfcVIj6oOwELF67pTOw66ubFOg9izMhkhC2XUPZTVnfSJd/AqncoY6sJcZmf2d4Ofr/2XqNePaZEao3F2awApO19KyNtQSIU0gQAOHo3w5+GYEFzJw/UA62QIDAQAB",
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoagMMS0YxVFjl0h6KEWNr/AxAN670jbNEuvbhh8jM9KsRzMxOb3irbtnaXyn0L4SsKE1E/sVQ+T2cChRUQG107bWjJLl80QU4hUtchsiOsZinWasJTgMRBB/fqRI9cRgjZlxlUuG2GPY0N+ULU7oK/XO2BkmJtosnGDTIppeGx0ALipRTux3ae7wEMF2FpI39vxKd6SYNO0Q8kNyNcCPa0UdhnjKSkbV/FDLUKv7916SEMZRfnKJQGzBQEL0BIp0iW9wvpl5I5WbN4/VhnmvO17m3yZy7cSHRf3oE5150pOaw1kIaYusHG9L8pYV+Cc4A6k+a5PrFgVoQe2ereRqGQIDAQAB",
   "auth-server-url": "http://localhost:8080/auth",
   "ssl-required": "external",
   resource: "my-react-client",
