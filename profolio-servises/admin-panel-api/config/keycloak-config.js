134befc4f74b0d15f45e10ccdcf0a328178a8464
Alexandr Dmowski <dmowski@yandex.ru>
2021-11-14 23:54:05 +0300

    Use env var for backend api

--- profolio-servises/admin-panel-api/config/keycloak-config.js
+++ profolio-servises/admin-panel-api/config/keycloak-config.js
@@ -3,13 +3,12 @@ var Keycloak = require("keycloak-connect");
 let _keycloak;
 
 var keycloakConfig = {
-  realm: "MyDemo",
-  "realm-public-key":
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoagMMS0YxVFjl0h6KEWNr/AxAN670jbNEuvbhh8jM9KsRzMxOb3irbtnaXyn0L4SsKE1E/sVQ+T2cChRUQG107bWjJLl80QU4hUtchsiOsZinWasJTgMRBB/fqRI9cRgjZlxlUuG2GPY0N+ULU7oK/XO2BkmJtosnGDTIppeGx0ALipRTux3ae7wEMF2FpI39vxKd6SYNO0Q8kNyNcCPa0UdhnjKSkbV/FDLUKv7916SEMZRfnKJQGzBQEL0BIp0iW9wvpl5I5WbN4/VhnmvO17m3yZy7cSHRf3oE5150pOaw1kIaYusHG9L8pYV+Cc4A6k+a5PrFgVoQe2ereRqGQIDAQAB",
-  "auth-server-url": "http://localhost:8080/auth",
-  "ssl-required": "external",
-  resource: "my-react-client",
-  "public-client": true,
+  realm: process.env.KEY_CLOAK_REALM_NAME,
+  "realm-public-key": process.env.KEY_CLOAK_REALM_PUBLIC_KEY,
+  "auth-server-url": process.env.KEY_CLOAK_AUTH_SERVER_URL,
+  "ssl-required": process.env.KEY_CLOAK_SSL_REQUIRED,
+  resource: process.env.KEY_CLOAK_CLIENT_ID,
+  "public-client": process.env.KEY_CLOAK_PUBLIC_CLIENT,
 };
 
 function initKeycloak(memoryStore) {
