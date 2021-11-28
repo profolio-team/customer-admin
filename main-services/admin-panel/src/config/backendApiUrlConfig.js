402bdbf5a4e8c6636a6dec57cf0d24ab7475fff5
Alexandr Dmowski <dmowski@yandex.ru>
2021-11-28 05:14:40 +0300

    Expand api of admin panel (UI)

--- main-services/admin-panel/src/config/backendApiUrlConfig.js
+++ main-services/admin-panel/src/config/backendApiUrlConfig.js
@@ -3,6 +3,11 @@ const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
 const apiUrls = {
   baseUrl,
   getUsersUrl: `${baseUrl}/users`,
+  customerRegistrationUrl: `${baseUrl}/customer-registration`,
+  customerGetAllUrl: `${baseUrl}/customer-get-all`,
+  customerDeleteOneUrl: `${baseUrl}/customer-delete-one`,
+  customerGetOneUrl: `${baseUrl}/customer-get-one`,
+  customerConfirmUrl: `${baseUrl}/customer-confirm`,
 };
 
 export default apiUrls;
