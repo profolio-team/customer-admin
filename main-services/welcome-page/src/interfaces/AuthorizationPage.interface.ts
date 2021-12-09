2ec75271c630f855c43ef967efdf432f455ea7a2
dllsc <92045376+dllsc@users.noreply.github.com>
2021-12-09 19:08:34 +0300

    pages + interface

---
+++ main-services/welcome-page/src/interfaces/AuthorizationPage.interface.ts
@@ -0,0 +1,3 @@
+export interface AuthorizationPageInterface {
+  form: 'CreateAccount' | 'signIn' | 'RecoveryPassword'
+}
---
+++ main-services/welcome-page/src/page/WelcomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '../component/Header/Header';
+import ContentWelcomePage from '../component/Content-welcome-page/Content-welcome-page';
+
+export default function WelcomePage() : JSX.Element{
+return <div>
+  <Header/>
+  <ContentWelcomePage/>
+</div>;
+}
