f2c3a1c9e492e754de3f7e6539d64b8393a23ac9
Alexandr Dmowski <dmowski@yandex.ru>
2022-01-14 20:12:54 +0300

    Use nestjs microservise boilerplate for customer backend api

--- customer-services/admin-panel-api/src/api/middlewares/auth.js
+++
@@ -1,6 +0,0 @@
-const { getKeycloak } = require('../../config/keycloak');
-
-exports.protect = (role) => (req, res, next) => {
-  const keyloack = getKeycloak();
-  keyloack.protect(role)(req, res, next);
-};
