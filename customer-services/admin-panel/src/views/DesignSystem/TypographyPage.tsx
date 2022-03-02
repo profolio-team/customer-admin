1852b615271c1300e8dfe0dbc436a6fcfab5292b
Игорь Запольский <56001649+shaugsa@users.noreply.github.com>
2022-03-02 20:42:44 +0300

    Typography (#33)
    * Update globals.css
    
    font-size css
    
    * typography
    
    change px / rem

--- customer-services/admin-panel/src/views/DesignSystem/TypographyPage.tsx
+++ customer-services/admin-panel/src/views/DesignSystem/TypographyPage.tsx
@@ -1,9 +1,16 @@
 export function TypographyPage(): JSX.Element {
   return (
     <div className="page-content">
-      <h1>Make your portfolio like a piece of cake</h1>
-      <h2>Make your portfolio like a piece of cake</h2>
-      <h3>Make your portfolio like a piece of cake</h3>
+      <p className="primary-display">Make your portfolio like a piece of cake</p>
+      <h1 className="primary-large">Make your portfolio like a piece of cake</h1>
+      <h2 className="primary-normal">Make your portfolio like a piece of cake</h2>
+      <h3 className="primary-small">Make your portfolio like a piece of cake</h3> 
+      <p className="body-normal">Make your portfolio like a piece of cake</p> 
+      <p className="body-small">Make your portfolio like a piece of cake</p> 
+      <p className="body-extra-small">Make your portfolio like a piece of cake</p> 
+      <a className="link-normal" href="#">Make your portfolio like a piece of cake</a> 
+      <br/>
+      <a className="link-small" href="#">Make your portfolio like a piece of cake</a> 
     </div>
   );
 }
