93e016d56f0d15768ee62c7d4ecb6198e7a98798
Alexandr Dmowski <dmowski@yandex.ru>
2022-03-06 00:05:59 +0300

    Emulators

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from "firebase-functions";
+import { db } from "./firebase";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest(async (request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
+  await db.collection("firstcollection").doc("doooc").set({
+    aaaa: "bbbbb",
+  });
   response.send("Hello from Firebase!");
 });
