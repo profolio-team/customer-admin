65482b3260ac85370676bb0cf4010a96728448ee
Alexandr Dmowski <dmowski@yandex.ru>
2022-03-06 14:11:50 +0300

    Move ev variables to github secrets

--- app/src/services/firebase/index.tsx
+++ app/src/services/firebase/index.tsx
@@ -1,19 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSz0tesRU2ZCgV1Zx6nPEZs58LU7ato1Q",
-  authDomain: "dev-profolio.firebaseapp.com",
-  databaseURL: "https://dev-profolio-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dev-profolio",
-  storageBucket: "dev-profolio.appspot.com",
-  messagingSenderId: "310851445929",
-  appId: "1:310851445929:web:810ce8caed0a54edeaf5b8",
-  measurementId: "G-25XJEE5QH4",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,5 +23,8 @@ const auth = getAuth(app);
 const storage = getStorage(app);
 const functions = getFunctions(app);
 const googleAuthProvider = new GoogleAuthProvider();
+if (process.env.REACT_APP_FIREBASE_EMULATOR === "true") {
+  connectFirestoreEmulator(firestore, "localhost", 8080);
+}
 
 export { auth, firestore, analytics, googleAuthProvider, storage, functions };
