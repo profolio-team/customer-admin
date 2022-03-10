cb23007a018bb8726bcbdf5cd647e0bfeb920fe4
Alexandr Dmowski <dmowski@yandex.ru>
2022-03-11 01:47:52 +0300

    Base page = typography

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./views/Home/home";
 import { InputsPage, ButtonsPage, TypographyPage, HeaderPage } from "./views/DesignSystem";
 import { Header } from "./components/core";
 import { ThemeContextProvider } from "./components/core/theme";
@@ -16,7 +15,7 @@ export default function App(): JSX.Element {
           <BrowserRouter>
             <Header />
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<TypographyPage />} />
               <Route path="/design-system-inputs" element={<InputsPage />} />
               <Route path="/design-system-buttons" element={<ButtonsPage />} />
               <Route path="/design-system-typography" element={<TypographyPage />} />
