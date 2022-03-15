bf4f26eed8400b5aea446bab38085a3a413c5815
Daniil Stakhavets <dlllstc@gmail.com>
2022-03-15 03:21:48 +0300

    fix4

--- app/src/components/core/textField/formLabel.style.ts
+++ app/src/components/core/textField/formLabel.style.ts
@@ -5,13 +5,13 @@ const style: ComponentsOverrides["MuiFormLabel"] = {
   root: {
     color: "var(--color-neutral-8)",
     fontSize: "0.875rem",
+    marginLeft: "4px",
     "&.Mui-focused": {
       color: "var(--color-neutral-8)",
     },
     "&.Mui-error": {
       color: "var(--color-neutral-8)",
     },
-    marginLeft: "4px",
   },
 };
 
--- app/src/views/DesignSystem/InputsPage.tsx
+++ app/src/views/DesignSystem/InputsPage.tsx
@@ -1,61 +1,60 @@
-import { Button, Stack, TextField } from "@mui/material";
+import { Box, Button, Container, Stack, TextField } from "@mui/material";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { AccountCircle } from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
 
 type Inputs = {
   example: string;
   exampleRequired: string;
+  exampleIcon: string;
 };
 
 export function InputsPage(): JSX.Element {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
-  console.log(watch("example"));
   const helper = errors.exampleRequired ? "Error message" : "Helper text";
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={2} width={240} padding={10}>
-        <TextField
-          label={"Label"}
-          helperText={"Helper text"}
-          defaultValue="Test"
-          {...register("example")}
-        />
-        <TextField
-          label={"Error field"}
-          {...register("exampleRequired", { required: true })}
-          error={!!errors.exampleRequired}
-          helperText={helper}
-        />
-        <TextField
-          InputProps={{
-            startAdornment: <AccountCircle />,
-          }}
-          label={"Label"}
-          helperText={"Helper text"}
-          defaultValue="test"
-          {...register("example")}
-        />
-        <TextField
-          InputProps={{
-            endAdornment: <AccountCircle />,
-          }}
-          label={"Label"}
-          helperText={"Helper text"}
-          defaultValue="test"
-          {...register("example")}
-        />
-        <Button variant={"outlined"} type="submit">
-          Get Error
-        </Button>
-      </Stack>
-    </form>
+    <Container maxWidth="xl" className="design-system-container">
+      <Box>
+        <Typography variant="h2" component="h2">
+          Inputs
+        </Typography>
+      </Box>
+      <hr />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2} width={240} padding={10}>
+          <TextField
+            label={"Label"}
+            helperText={"Helper text"}
+            placeholder={"Placeholder"}
+            {...register("example")}
+          />
+          <TextField
+            label={"Error field"}
+            {...register("exampleRequired", { required: true })}
+            placeholder={"Placeholder"}
+            error={!!errors.exampleRequired}
+            helperText={helper}
+          />
+          <TextField
+            InputProps={{
+              startAdornment: <AccountCircle />,
+            }}
+            label={"Label"}
+            helperText={"Helper text"}
+            placeholder={"Placeholder"}
+            {...register("exampleIcon")}
+          />
+          <Button variant={"outlined"} type="submit">
+            Get Error
+          </Button>
+        </Stack>
+      </form>
+    </Container>
   );
 }
