5ba786930ba344d009c7422f0b16c57bf0b9ab87
Daniil Stakhavets <dlllstc@gmail.com>
2022-03-21 05:16:14 +0300

    fix 0.5 User Info connected db

--- app/src/views/UserInfo/UserInfoPage.tsx
+++ app/src/views/UserInfo/UserInfoPage.tsx
@@ -6,43 +6,51 @@ import { useAuth } from "../../hooks/useAuth";
 import { Inputs, UserInfoForm } from "./userInfoForm";
 
 export function UserInfoPage(): JSX.Element {
-  const [preloadedValues, setPreloadedValues] = useState<Inputs | null>(null);
-  const { isAuthorized, uid, userInfo } = useAuth();
+  const [readyForDisplay, setReady] = useState(false);
+
+  const [preloadedValues, setPreloadedValues] = useState<Inputs>({
+    firstName: "",
+    about: "",
+    email: "",
+    lastName: "",
+    linkedIn: "",
+    phone: "",
+    avatar: "",
+  });
+
+  const { isAuthorized, uid, userInfo, loading: loadingAuth, user } = useAuth();
+
   const [userInfoDB, loading] = useDocumentData(isAuthorized ? doc(db.users, uid) : null, {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
 
   useEffect(() => {
-    console.log(loading);
-    if (!loading) {
-      console.log("loading end");
-      if (userInfoDB) {
-        console.log("userDB true");
-        setPreloadedValues({
-          firstName: userInfoDB.firstName || "",
-          about: userInfoDB.about || "",
-          email: userInfo.email || "",
-          lastName: userInfoDB.lastName || "",
-          linkedIn: userInfoDB.linkedInUrl || "",
-          phone: userInfoDB.phone || "",
-        });
-      } else {
-        console.log("userDB false");
-        setPreloadedValues({
-          firstName: "",
-          about: "",
-          email: userInfo.email || "",
-          lastName: "",
-          linkedIn: "",
-          phone: "",
-        });
-      }
+    if (!loadingAuth) {
+      setPreloadedValues({
+        ...preloadedValues,
+        email: userInfo.email || "",
+      });
     }
-    console.log(preloadedValues);
+
+    if (loading || loadingAuth) {
+      return;
+    }
+    if (userInfoDB) {
+      setPreloadedValues({
+        avatar: user?.photoURL || "",
+        firstName: userInfoDB.firstName || "",
+        about: userInfoDB.about || "",
+        email: userInfo.email || "",
+        lastName: userInfoDB.lastName || "",
+        linkedIn: userInfoDB.linkedInUrl || "",
+        phone: userInfoDB.phone || "",
+      });
+    }
+    setReady(true);
   }, [loading]);
 
-  return !loading && preloadedValues ? (
-    <UserInfoForm preloadedValues={preloadedValues} />
+  return readyForDisplay && user ? (
+    <UserInfoForm user={user} preloadedValues={preloadedValues} uid={uid} />
   ) : (
     <>Loading...</>
   );
