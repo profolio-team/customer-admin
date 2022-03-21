143cb5a92fea0ed0b27bb33b2490eebc0deb577d
Alexandr Dmowski <dmowski@yandex.ru>
2022-03-21 18:24:45 +0300

    Update routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,12 +7,16 @@ import {
   CheckboxesPage,
   HeaderPage,
 } from "./views/DesignSystem";
-import { Header } from "./components";
+import { StaticHeader, UserHeader } from "./components";
 import { ThemeContextProvider } from "./components/core/theme";
 import { FirestorePage } from "./views/Firestore/FirestorePage";
 import { AuthProvider } from "./hooks/useAuth";
 import { ConfigProvider } from "./hooks/config";
 import { UserInfoPage } from "./views/UserInfo/UserInfoPage";
+import { NotFoundPage } from "./views/Error/NotFoundPage";
+import { ContactsPage } from "./views/Contacts/Contacts";
+import { ExamplesPage } from "./views/Examples/Examples";
+import { DashboardPage } from "./views/Dashboard/Dashboard";
 
 export default function App(): JSX.Element {
   return (
@@ -20,9 +24,9 @@ export default function App(): JSX.Element {
       <BrowserRouter>
         <AuthProvider>
           <ConfigProvider>
-            <Header />
+            <UserHeader />
             <Routes>
-              <Route path="/" element={<TypographyPage />} />
+              <Route path="/" element={<DashboardPage />} />
               <Route path="/design-system-inputs" element={<InputsPage />} />
               <Route path="/design-system-buttons" element={<ButtonsPage />} />
               <Route path="/design-system-typography" element={<TypographyPage />} />
@@ -30,6 +34,25 @@ export default function App(): JSX.Element {
               <Route path="/design-system-header" element={<HeaderPage />} />
               <Route path="/firestore" element={<FirestorePage />} />
               <Route path="/user-info" element={<UserInfoPage />} />
+              <Route
+                path="/contacts"
+                element={
+                  <>
+                    <StaticHeader />
+                    <ContactsPage />
+                  </>
+                }
+              />
+              <Route
+                path="/examples"
+                element={
+                  <>
+                    <StaticHeader />
+                    <ExamplesPage />
+                  </>
+                }
+              />
+              <Route path="*" element={<NotFoundPage />} />
             </Routes>
           </ConfigProvider>
         </AuthProvider>
