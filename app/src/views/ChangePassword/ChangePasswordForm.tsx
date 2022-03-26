d145de7ca00409a78da4351eb71310df5d71f455
Daniil Stakhavets <dlllstc@gmail.com>
2022-03-27 00:05:07 +0300

    fix submit password

--- app/src/views/ChangePassword/ChangePasswordForm.tsx
+++ app/src/views/ChangePassword/ChangePasswordForm.tsx
@@ -12,9 +12,10 @@ import {
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { User } from "firebase/auth";
-import { updatePassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { updatePassword, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
 import { ErrorMessage } from "@hookform/error-message";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { FirebaseError } from "@firebase/util";
 
 interface IChangePasswordForm {
   oldPassword: string;
@@ -28,7 +29,6 @@ interface ChangePasswordFormProps {
 
 export function ChangePasswordForm({ user }: ChangePasswordFormProps): JSX.Element {
   const navigate = useNavigate();
-  const auth = getAuth();
   const {
     register,
     setError,
@@ -53,28 +53,32 @@ export function ChangePasswordForm({ user }: ChangePasswordFormProps): JSX.Eleme
     navigate("/");
   };
 
-  const onSubmit: SubmitHandler<IChangePasswordForm> = async (data) => {
-    const checkPasswordOnServer = await signInWithEmailAndPassword(
-      auth,
-      user.email || "",
-      data.oldPassword
-    ).catch((error) => {
-      const errorCode = error.code;
-      if (errorCode === "auth/wrong-password") {
-        setError("oldPassword", { message: "Wrong password" }, { shouldFocus: true });
+  const postNewPassword = async (oldPassword: string, newPassword: string) => {
+    const credential = EmailAuthProvider.credential(user.email || "", oldPassword);
+    await reauthenticateWithCredential(user, credential);
+    await updatePassword(user, newPassword);
+  };
+
+  const errorHandlerSubmitPassword = (error: unknown) => {
+    if (error instanceof FirebaseError) {
+      if (error.code === "auth/wrong-password") {
+        return "Error: wrong password";
       } else {
-        setError("oldPassword", { message: errorCode }, { shouldFocus: true });
+        return "Error: ???";
       }
-    });
-    if (checkPasswordOnServer) {
-      updatePassword(user, data.newPassword)
-        .then(() => {
-          navigate("/");
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          setError("oldPassword", { message: errorCode }, { shouldFocus: true });
-        });
+    }
+  };
+
+  const onSubmit: SubmitHandler<IChangePasswordForm> = async (data) => {
+    try {
+      await postNewPassword(data.oldPassword, data.newPassword);
+      navigate("/");
+    } catch (error) {
+      setError(
+        "oldPassword",
+        { message: errorHandlerSubmitPassword(error) },
+        { shouldFocus: true }
+      );
     }
   };
 
