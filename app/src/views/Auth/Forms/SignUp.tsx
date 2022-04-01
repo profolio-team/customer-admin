b512d73095e6c9c759467883c978ed88d0899852
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-02 00:15:41 +0300

    registration template

--- app/src/views/Auth/Forms/SignUp.tsx
+++ app/src/views/Auth/Forms/SignUp.tsx
@@ -1,10 +1,10 @@
 import Button from "@mui/material/Button";
-import { Box, TextField, Typography } from "@mui/material";
-import { auth, signInByGoogle } from "../../../services/firebase";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { useState } from "react";
+import { Box, Link, Stack, TextField, Typography } from "@mui/material";
+import { functions, signInByGoogle } from "../../../services/firebase";
+import { useEffect, useState } from "react";
 import GoogleIcon from "@mui/icons-material/Google";
 import { ExternalServiceSignIn } from "../style";
+import { httpsCallable } from "firebase/functions";
 
 const formatErrorMessage = (errorMessage: string) => {
   errorMessage = errorMessage.replace("Firebase: Error (auth/", "");
@@ -15,19 +15,68 @@ const formatErrorMessage = (errorMessage: string) => {
 };
 
 export function SignUpForm(): JSX.Element {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("dmowski@yandex.ru");
+  const [password, setPassword] = useState(Date.now() + "password");
+  const [domain, setDomain] = useState("company" + Date.now() + "end");
+  const [error, setError] = useState("");
+  const [isVerifyEmail, verifyEmailMode] = useState(false);
 
-  const [createUserWithEmailAndPassword, , loadingCreate, errorCreate] =
-    useCreateUserWithEmailAndPassword(auth);
+  const registerCompany = httpsCallable(functions, "registerCompany");
 
-  if (loadingCreate) {
-    return <p>Loading...</p>;
-  }
-
-  const signUp = () => {
-    createUserWithEmailAndPassword(email, password);
+  const signUp = async () => {
+    const resultFromFunction = await registerCompany({ email, domain });
+    const { result, error } = resultFromFunction.data as { result: string; error: string };
+    if (error) {
+      setError(error);
+    } else {
+      verifyEmailMode(true);
+      setError("");
+    }
+    console.log("result of registerCompany", result);
   };
+
+  useEffect(() => {
+    const isSubDomain = location.host.split(".").length > 1;
+    if (isSubDomain) {
+      const urlWithoutSubdomain = location
+        .toString()
+        .replace(location.host.split(".")[0] + ".", "");
+
+      window.location.href = urlWithoutSubdomain;
+    }
+  }, []);
+
+  if (isVerifyEmail) {
+    return (
+      <>
+        <Stack gap={3} sx={{ maxWidth: "400px", paddingBottom: "3rem" }}>
+          <Typography
+            variant="h2"
+            component="h2"
+            sx={{
+              textAlign: "center",
+            }}
+          >
+            Please verify your email
+          </Typography>
+
+          <Typography variant="body1" component="p">
+            You're almost there! We sent an email to <b>{email}</b>. Just click on the link in that
+            email to complete your sign up.
+          </Typography>
+
+          <Typography variant="body1" component="p">
+            If you don't see the email, please check it under your junk, spam, social or other
+            folders.
+          </Typography>
+
+          <Link href="#" variant="body2">
+            I didn't receive the email.
+          </Link>
+        </Stack>
+      </>
+    );
+  }
 
   return (
     <>
@@ -52,7 +101,6 @@ export function SignUpForm(): JSX.Element {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <TextField
           id="password"
           type="password"
@@ -62,9 +110,18 @@ export function SignUpForm(): JSX.Element {
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        {errorCreate?.message && (
+        <TextField
+          id="domain"
+          type="text"
+          placeholder="Enter domain name"
+          label={"Domain"}
+          value={domain}
+          onChange={(e) => setDomain(e.target.value)}
+        />
+
+        {error && (
           <p style={{ color: "var(--color-functional-error)" }}>
-            Error: {formatErrorMessage(errorCreate?.message || "")}
+            Error: {formatErrorMessage(error || "")}
           </p>
         )}
 
