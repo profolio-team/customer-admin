f9547b0514c0bb1676baf80a52536ac2943a9479
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-02 22:59:12 +0300

    Fix notification resolve conflcts

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,12 +6,14 @@ import {
   TypographyPage,
   CheckboxesPage,
   HeaderPage,
+  DialogPage,
   SnackbarPage,
 } from "./views/DesignSystem";
-import { StaticHeader, UserHeader } from "./components";
+import { Header } from "./components";
 import { ThemeContextProvider } from "./components/core/theme";
 import { FirestorePage } from "./views/Firestore/FirestorePage";
 import { AuthProvider } from "./hooks/useAuth";
+import { DialogProvider } from "./hooks/useDialog";
 import { ConfigProvider } from "./hooks/config";
 
 import { UserInfoPage } from "./views/UserInfo/UserInfoPage";
@@ -19,49 +21,45 @@ import { NotFoundPage } from "./views/Error/NotFoundPage";
 import { ContactsPage } from "./views/Contacts/Contacts";
 import { ExamplesPage } from "./views/Examples/Examples";
 import { DashboardPage } from "./views/Dashboard/Dashboard";
+import { TableDesign } from "./views/DesignSystem/TableDesign";
+import { ChangePasswordPage } from "./views/ChangePassword/ChangePasswordPage";
 
 import { NotificationProvider } from "./hooks/useNotification";
+
 export default function App(): JSX.Element {
+  const routesComponent = (
+    <Routes>
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/design-system-inputs" element={<InputsPage />} />
+      <Route path="/design-system-buttons" element={<ButtonsPage />} />
+      <Route path="/design-system-typography" element={<TypographyPage />} />
+      <Route path="/design-system-checkboxes" element={<CheckboxesPage />} />
+      <Route path="/design-system-header" element={<HeaderPage />} />
+      <Route path="/design-system-dialog" element={<DialogPage />} />
+      <Route path="/design-system-table" element={<TableDesign />} />
+      <Route path="/design-system-snackbar" element={<SnackbarPage />} />
+      <Route path="/firestore" element={<FirestorePage />} />
+      <Route path="/user-info" element={<UserInfoPage />} />
+      <Route path="/contacts" element={<ContactsPage />} />
+      <Route path="/examples" element={<ExamplesPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/change-password" element={<ChangePasswordPage />} />
+    </Routes>
+  );
+
   return (
     <ThemeContextProvider>
       <NotificationProvider>
-        <BrowserRouter>
-          <AuthProvider>
-            <ConfigProvider>
-              <UserHeader />
-              <Routes>
-                <Route path="/" element={<DashboardPage />} />
-                <Route path="/design-system-inputs" element={<InputsPage />} />
-                <Route path="/design-system-buttons" element={<ButtonsPage />} />
-                <Route path="/design-system-typography" element={<TypographyPage />} />
-                <Route path="/design-system-checkboxes" element={<CheckboxesPage />} />
-                <Route path="/design-system-header" element={<HeaderPage />} />
-                <Route path="/design-system-snackbar" element={<SnackbarPage />} />
-                <Route path="/firestore" element={<FirestorePage />} />
-                <Route path="/user-info" element={<UserInfoPage />} />
-                <Route
-                  path="/contacts"
-                  element={
-                    <>
-                      <StaticHeader />
-                      <ContactsPage />
-                    </>
-                  }
-                />
-                <Route
-                  path="/examples"
-                  element={
-                    <>
-                      <StaticHeader />
-                      <ExamplesPage />
-                    </>
-                  }
-                />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </ConfigProvider>
-          </AuthProvider>
-        </BrowserRouter>
+        <DialogProvider>
+          <BrowserRouter>
+            <AuthProvider>
+              <ConfigProvider>
+                <Header />
+                {routesComponent}
+              </ConfigProvider>
+            </AuthProvider>
+          </BrowserRouter>
+        </DialogProvider>
       </NotificationProvider>
     </ThemeContextProvider>
   );
