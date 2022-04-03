7f686f411cfbbd710cb64ef9edfc6911fce814c7
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-03 14:31:01 +0300

    Redirect to signin page if user does not logined

--- app/src/hooks/useAuth.tsx
+++ app/src/hooks/useAuth.tsx
@@ -1,6 +1,7 @@
 import { signOut, User } from "firebase/auth";
-import { Context, createContext, ReactNode, useContext } from "react";
+import { Context, createContext, ReactNode, useContext, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 import { auth } from "../services/firebase";
 
 interface AuthContext {
@@ -15,6 +16,7 @@ interface AuthContext {
   user: User | null;
   isAuthorized: boolean;
   logout: () => Promise<void>;
+  redirectToSignIn: () => Promise<void>;
 }
 
 const authContext: Context<AuthContext> = createContext<AuthContext>({
@@ -28,11 +30,13 @@ const authContext: Context<AuthContext> = createContext<AuthContext>({
     displayName: "",
   },
   user: null,
+  redirectToSignIn: async () => void 0,
   logout: async () => void 0,
 });
 
 function useProvideAuth(): AuthContext {
   const [user, loading, errorAuth] = useAuthState(auth);
+  const navigate = useNavigate();
   const isAuthorized = !!user?.uid && !errorAuth;
   const userInfo = {
     email: user?.email || "",
@@ -45,6 +49,11 @@ function useProvideAuth(): AuthContext {
 
   const logout = async (): Promise<void> => {
     await signOut(auth);
+    await redirectToSignIn();
+  };
+
+  const redirectToSignIn = async (): Promise<void> => {
+    await navigate("/sign-in");
   };
 
   return {
@@ -52,6 +61,7 @@ function useProvideAuth(): AuthContext {
     userInfo,
     uid: user?.uid || "",
     loading,
+    redirectToSignIn,
     logout,
     user: user || null,
   };
@@ -59,7 +69,22 @@ function useProvideAuth(): AuthContext {
 
 export function AuthProvider(props: { children: ReactNode }): JSX.Element {
   const auth = useProvideAuth();
+  const location = useLocation();
   const componentForShow = props.children;
+  const staticPages = ["/contacts", "/examples", "/redirect"];
+  const authPages = ["/sign-in", "/sign-up", "/restore-password"];
+
+  const pathname = location.pathname;
+  const isStaticPage = staticPages.includes(pathname);
+  const isAuthPage = authPages.includes(pathname);
+
+  useEffect(() => {
+    if (!auth.loading && !auth.isAuthorized && !isAuthPage && !isStaticPage) {
+      setTimeout(() => {
+        auth.redirectToSignIn();
+      }, 0);
+    }
+  }, [isStaticPage, isAuthPage, auth.isAuthorized, auth.loading]);
 
   if (auth.loading) {
     return <></>;
