bbcd7dc174834e82eab4caca0983950e52572462
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-03 14:30:26 +0300

    Custom header for company/root mode

--- app/src/components/Header/StaticHeader.tsx
+++ app/src/components/Header/StaticHeader.tsx
@@ -1,12 +1,22 @@
+import { isExtendedUrl } from "../../utils/url.utils";
 import BaseHeader, { HeaderMenuElement } from "./BaseHeader";
 
 export function StaticHeader(): JSX.Element {
-  const menuItems: HeaderMenuElement[] = [
-    { linkTo: "/examples", title: "Examples", type: "Tab" },
-    { linkTo: "/contacts", title: "Contact Us", type: "Tab" },
-    { linkTo: "/sign-in", title: "Sign In", type: "Tab" },
-    { linkTo: "/sign-up", title: "Create Account", type: "Button" },
-  ];
+  let menuItems: HeaderMenuElement[] = [];
+
+  if (isExtendedUrl) {
+    menuItems = [
+      { linkTo: "/contacts", title: "Contact Us", type: "Tab" },
+      { linkTo: "/sign-in", title: "Sign In", type: "Tab" },
+    ];
+  } else {
+    menuItems = [
+      { linkTo: "/examples", title: "Examples", type: "Tab" },
+      { linkTo: "/contacts", title: "Contact Us", type: "Tab" },
+      { linkTo: "/sign-in", title: "Sign In", type: "Tab" },
+      { linkTo: "/sign-up", title: "Create Account", type: "Button" },
+    ];
+  }
 
   return <BaseHeader rightHeaderMenuItems={menuItems} />;
 }
