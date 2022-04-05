19dfdce3ac741d04fe0fddbe44498f54a5ea67cc
Anton Demeshkevich <demeshkevichanton@gmail.com>
2022-04-06 00:21:14 +0300

    refact

---
+++ app/src/components/core/circularProgress.style.ts
@@ -0,0 +1,6 @@
+import { GetTypeOfProperty } from "../../utils/type.utils";
+import { Components } from "@mui/material";
+
+export const customCircularProgress: GetTypeOfProperty<Components, "MuiCircularProgress"> = {
+  defaultProps: { disableShrink: true },
+};
