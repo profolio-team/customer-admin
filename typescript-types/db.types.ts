385a895352840ce1c28d2da60b343c1bec574239
Daniil Stakhavets <dlllstc@gmail.com>
2022-04-06 15:52:50 +0300

    add interface CustomClaim in db types

--- typescript-types/db.types.ts
+++ typescript-types/db.types.ts
@@ -1,16 +1,23 @@
 export interface UserInfo {
-  firstName?: string;
-  lastName?: string;
-  linkedInUrl?: string;
-  about?: string;
-  phone?: string;
+  firstName: string;
+  lastName: string;
+  linkedInUrl: string;
+  about: string;
+  phone: string;
+  email: string;
 }
 
 export interface CompanyInfo {
-  name?: string;
-  email?: string;
-  logoUrl?: string;
-  about?: string;
-  phone?: string;
-  template?: string;
+  name: string;
+  email: string;
+  logoUrl: string;
+  about: string;
+  phone: string;
+  template: string;
+}
+
+export interface CustomClaims {
+  domain: string;
+  isAdmin: boolean;
+  isOwner?: boolean;
 }
