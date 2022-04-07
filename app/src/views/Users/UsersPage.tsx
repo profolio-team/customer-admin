0e9f807cf1c24844a22a925cdc89c5d6925074a1
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-07 22:37:00 +0300

    columns for users grid

--- app/src/views/Users/UsersPage.tsx
+++ app/src/views/Users/UsersPage.tsx
@@ -6,6 +6,11 @@ import { Loader } from "../../components";
 import db from "../../services/firebase/firestore";
 import { UserInfo } from "../../../../typescript-types/db.types";
 
+interface UserColumnInterface {
+  title: string;
+  field: keyof UserInfo;
+}
+
 export function UsersPage() {
   const navigate = useNavigate();
   const [users] = useCollection(db.users);
@@ -17,10 +22,17 @@ export function UsersPage() {
   users.docs.map((user) => {
     data.push(user.data());
   });
+  const columns: UserColumnInterface[] = [
+    { title: "Email", field: "email" },
+    { title: "First Name", field: "firstName" },
+    { title: "Last Name", field: "lastName" },
+    { title: "Phone", field: "phone" },
+  ];
+
   return (
     <Container>
       <Button onClick={() => navigate("create")}>Create New User</Button>
-      <MaterialTable columns={[{ title: "email", field: "email" }]} data={data} />
+      <MaterialTable columns={columns} data={data} />
     </Container>
   );
 }
