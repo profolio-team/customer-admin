ea3d7946a728849af610ba810b2f92c5875fe78f
Dmowski Alexandr <dmowski@yandex.ru>
2022-04-08 14:24:15 +0300

    Refactoring user grid (#78)
    * Refactoring getting user info
    
    * columns for users grid
    
    * Refactoring interfaces for userPage
    
    * Fix templates

--- app/src/views/Users/UsersPage.tsx
+++ app/src/views/Users/UsersPage.tsx
@@ -1,28 +1,45 @@
 import MaterialTable from "material-table";
-import { Button, Container } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection } from "firebase/firestore";
-import { firestore } from "../../services/firebase";
-import { companyName } from "../../utils/url.utils";
-import { DocumentData } from "@firebase/firestore-types";
 import { Loader } from "../../components";
+import db from "../../services/firebase/firestore";
+import { UserInfo } from "../../../../typescript-types/db.types";
+
+interface UserColumn {
+  title: string;
+  field: keyof UserInfo;
+}
 
 export function UsersPage() {
   const navigate = useNavigate();
-  const [users] = useCollection(collection(firestore, `companies/${companyName}/users`));
-  const data: DocumentData[] = [];
+  const [usersCollection] = useCollection(db.users);
 
-  if (!users) {
+  if (!usersCollection) {
     return <Loader />;
   }
-  users.docs.map((user) => {
-    data.push(user.data());
-  });
+
+  const users: UserInfo[] = usersCollection.docs.map((usersDoc) => usersDoc.data());
+
+  const columns: UserColumn[] = [
+    { title: "Email", field: "email" },
+    { title: "First Name", field: "firstName" },
+    { title: "Last Name", field: "lastName" },
+    { title: "Phone", field: "phone" },
+  ];
+
   return (
-    <Container>
-      <Button onClick={() => navigate("create")}>Create New User</Button>
-      <MaterialTable columns={[{ title: "email", field: "email" }]} data={data} />
+    <Container maxWidth="xl" sx={{ padding: "2rem 0" }}>
+      <Stack direction={"row"} sx={{ padding: "2rem 0" }} justifyContent={"space-between"}>
+        <Typography variant="h2" component="h2">
+          Users
+        </Typography>
+        <Button variant="contained" onClick={() => navigate("create")}>
+          Create New User
+        </Button>
+      </Stack>
+
+      <MaterialTable title={"Users"} columns={columns} data={users} />
     </Container>
   );
 }
