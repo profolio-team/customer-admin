6723b921537bc907afc1c9f46ba2a3dab9333f9b
Daniil Stakhavets <dlllstc@gmail.com>
2022-04-09 12:36:23 +0300

    fix background autofill

--- app/src/components/core/textField/input.style.ts
+++ app/src/components/core/textField/input.style.ts
@@ -2,6 +2,12 @@ import { Components, ComponentsOverrides } from "@mui/material";
 import { GetTypeOfProperty } from "../../../utils/type.utils";
 
 const style: ComponentsOverrides["MuiInputBase"] = {
+  input: {
+    "&:-webkit-autofill": {
+      transitionDelay: "9999s",
+      transitionProperty: "background-color, color",
+    },
+  },
   root: {
     backgroundColor: "var(--color-neutral-1)",
     "label + &": {
