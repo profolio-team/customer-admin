e09f76bde4b072fbeed75b4ce389bb4dab90df6d
Alexandr Dmowski <dmowski@yandex.ru>
2022-04-13 20:26:33 +0300

    Fix autofill input

--- app/src/components/core/textField/input.style.ts
+++ app/src/components/core/textField/input.style.ts
@@ -2,6 +2,14 @@ import { Components, ComponentsOverrides } from "@mui/material";
 import { GetTypeOfProperty } from "../../../utils/type.utils";
 
 const style: ComponentsOverrides["MuiInputBase"] = {
+  input: {
+    "&:-webkit-autofill": {
+      boxShadow: "inset 0 0 0 30px var( --color-input-autofocus)",
+      "& + button": {
+        marginLeft: "-52px",
+      },
+    },
+  },
   root: {
     backgroundColor: "var(--color-neutral-1)",
     "label + &": {
