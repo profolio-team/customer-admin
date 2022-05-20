4d718ecb16447ef4db9a30707091de2f5ab32856
Dmowski Alexandr <dmowski@yandex.ru>
2022-05-20 23:13:46 +0300

    Update cron job (#111)

--- functions/src/test/cronExample.ts
+++ functions/src/test/cronExample.ts
@@ -1,7 +1,7 @@
 import { firebaseFunctions } from "../firebase";
 
 export const exampleOfCronJob = firebaseFunctions.pubsub
-  .schedule("every 10 minutes")
+  .schedule("every 10 hours")
   .onRun(async () => {
     console.log("This action is happening 1 time per 10 min");
   });
