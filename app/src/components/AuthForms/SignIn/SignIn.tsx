e0c6f43ed84b258dd8c8e2e0c2437a6d8c256228
dllsc <92045376+dllsc@users.noreply.github.com>
2022-06-07 23:51:50 +0300

    smart and dumb component

--- app/src/components/AuthForms/SignIn/SignIn.tsx
+++ app/src/components/AuthForms/SignIn/SignIn.tsx
@@ -3,166 +3,75 @@ import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useEffect, useState } from "react";
 import { httpsCallable } from "firebase/functions";
 import {
-  companyName,
   getEmailParamFromUrl,
   getPasswordParamFromUrl,
   redirectToSignInPage,
 } from "../../../utils/url.utils";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../hooks/useAuth";
-import { Loader } from "../../index";
 import {
   GetUserDomainByEmailRequest,
   GetUserDomainByEmailResponse,
 } from "../../../../../functions/src/callable/user/getUserDomainByEmail";
-
-import { Box, Button, TextField } from "@mui/material";
-import { parseFirebaseErrorMessage } from "../../../services/firebase/errorMessages";
-import { AuthTitle, ErrorInfo } from "../style";
+import { SignInForm } from "../../Forms/SignInForm";
 import { SelectDomain } from "./SelectDomain";
-import { SubmitHandler, useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { VALIDATORS } from "../../../utils/formValidator";
 
 const getUserDomain = httpsCallable<GetUserDomainByEmailRequest, GetUserDomainByEmailResponse>(
   functions,
   "user-getUserDomainByEmail"
 );
 
-type ISignIn = {
-  email: string;
-  password: string;
-};
-
 export function SignIn(): JSX.Element {
   const navigate = useNavigate();
 
   const emailFromUrl = getEmailParamFromUrl();
   const passwordFromUrl = getPasswordParamFromUrl();
-
-  const schemaEmail = yup.object({
-    email: yup.string().required("Email is required to enter").email("Email is incorrect"),
-  });
-
-  const [domain, setDomain] = useState(companyName || "");
-
   const [domainList, setDomainList] = useState<string[]>([]);
-
-  const { isAuthorized, loading: authLoading } = useAuth();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const { isAuthorized } = useAuth();
+  const [email, setEmail] = useState("");
 
   const [signInWithEmailAndPassword, , , errorLogin] = useSignInWithEmailAndPassword(auth);
 
-  const schemaPassword = yup.object({
-    password: yup
-      .string()
-      .required(VALIDATORS.PASSWORD.required)
-      .max(VALIDATORS.PASSWORD.maxLength.value, VALIDATORS.PASSWORD.maxLength.message)
-      .min(VALIDATORS.PASSWORD.minLength.value, VALIDATORS.PASSWORD.minLength.message),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<ISignIn>({
-    resolver: yupResolver(emailFromUrl ? schemaPassword : schemaEmail),
-    defaultValues: {
-      email: emailFromUrl,
-      password: passwordFromUrl,
-    },
-  });
-
-  useEffect(() => {
-    if (errorLogin?.message) {
-      setError(parseFirebaseErrorMessage(errorLogin?.message));
-      setLoading(false);
-    }
-  }, [errorLogin]);
-
   useEffect(() => {
     if (isAuthorized) {
-      setLoading(true);
       setTimeout(() => {
         navigate("/");
       }, 100);
     }
   }, [isAuthorized]);
 
-  const signIn = async ({ email, password }: ISignIn): Promise<void> => {
-    setLoading(true);
+  const redirect = (selectDomain: string) => {
+    redirectToSignInPage({ domain: selectDomain, email });
+  };
 
+  const postEmail = async (email: string): Promise<void> => {
     const userDomainInfo = await getUserDomain({ email });
     const { domains, error } = userDomainInfo.data;
     if (error) {
-      setError(error);
-      setLoading(false);
-      return;
+      return new Promise((resolve, reject) => {
+        reject(new Error(error));
+      });
     }
-
-    if (!domain && domains.length > 1) {
+    if (domains.length > 1) {
       setDomainList(domains);
-      setLoading(false);
+      setEmail(email);
       return;
     }
-
-    const domainForRedirect = domain || domains[0];
-    if (redirectToSignInPage({ domain: domainForRedirect, email })) {
-      return;
-    }
-
-    await signInWithEmailAndPassword(email, password);
+    redirectToSignInPage({ domain: domains[0], email });
   };
-
-  if (authLoading || loading) {
-    return <Loader />;
+  if (domainList.length > 1) {
+    return <SelectDomain domainList={domainList} redirect={redirect} />;
+  }
+  if (!emailFromUrl) {
+    return <SignInForm signIn={postEmail} />;
   }
 
-  const onSubmit: SubmitHandler<ISignIn> = (data) => {
-    signIn(data);
-  };
-
   return (
-    <>
-      <AuthTitle>Sign In</AuthTitle>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-          <TextField
-            id="email"
-            type="email"
-            placeholder="Enter corporate email"
-            label={"Email address"}
-            hidden={!!emailFromUrl}
-            {...register("email")}
-            error={!!errors.email?.message}
-            helperText={errors.email?.message}
-          />
-
-          {!companyName && (
-            <SelectDomain domainList={domainList} selectedDomain={domain} onChange={setDomain} />
-          )}
-
-          <TextField
-            id="password"
-            placeholder="Enter password"
-            label={"Password"}
-            type="password"
-            hidden={!emailFromUrl}
-            {...register("password")}
-            error={!!errors.password?.message}
-            helperText={errors.password?.message}
-          />
-
-          {error && <ErrorInfo>{error}</ErrorInfo>}
-
-          <Button variant="contained" type="submit" sx={{ marginTop: "1rem" }}>
-            {!emailFromUrl ? "Continue" : "Sign In"}
-          </Button>
-        </Box>
-      </form>
-    </>
+    <SignInForm
+      signIn={signInWithEmailAndPassword}
+      emailFromUrl={emailFromUrl}
+      passwordFromUrl={passwordFromUrl}
+      error={errorLogin}
+    />
   );
 }
