e9a6e09ed3ebd6df4755c40f22489a25dca7499a
Alexandr Dmowski <dmowski@yandex.ru>
2022-06-18 21:07:44 +0300

    Simplify input storybook

--- app/src/stories/Input.stories.tsx
+++ app/src/stories/Input.stories.tsx
@@ -1,58 +1,28 @@
-import { Button, Stack, TextField } from "@mui/material";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { TextField } from "@mui/material";
 import { AccountCircle } from "@mui/icons-material";
 import { InputsHelper } from "../components/InputsHelper/InputsHelper";
-
-type Inputs = {
-  example: string;
-  exampleRequired: string;
-  exampleIcon: string;
-};
 
 export default {
   title: "Components/Inputs",
 };
 
-export function InputTemplate(): JSX.Element {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-  const helper = errors.exampleRequired ? "Error message" : "Helper text";
+export const InputWithIcon = () => (
+  <TextField
+    label={"Label"}
+    helperText={"Helper text"}
+    placeholder={"Placeholder"}
+    InputProps={{
+      startAdornment: <AccountCircle />,
+    }}
+  />
+);
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={2} width={240}>
-        <TextField
-          label={"Label"}
-          helperText={"Helper text"}
-          placeholder={"Placeholder"}
-          {...register("example")}
-        />
-        <TextField
-          label={"Error field"}
-          {...register("exampleRequired", { required: true })}
-          placeholder={"Placeholder"}
-          error={!!errors.exampleRequired}
-          helperText={helper}
-        />
-        <TextField
-          InputProps={{
-            startAdornment: <AccountCircle />,
-          }}
-          label={"Label"}
-          helperText={"Helper text"}
-          placeholder={"Placeholder"}
-          {...register("exampleIcon")}
-        />
-        <InputsHelper />
-        <Button variant={"outlined"} type="submit">
-          Get Error
-        </Button>
-      </Stack>
-    </form>
-  );
-}
-InputTemplate.storyName = "Inputs";
+export const Input = () => (
+  <TextField label={"Label"} helperText={"Helper text"} placeholder={"Placeholder"} />
+);
+
+export const InputError = () => (
+  <TextField label={"Label"} error helperText={"Helper text"} placeholder={"Placeholder"} />
+);
+
+export const InputsHelperComponent = () => <InputsHelper />;
