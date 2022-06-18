90d9c2589737f8239daf4bef3fba0dc0a84ac915
Alexandr Dmowski <dmowski@yandex.ru>
2022-06-18 20:45:37 +0300

    Simplify checboxes

--- app/src/stories/Checkbox.stories.tsx
+++ app/src/stories/Checkbox.stories.tsx
@@ -1,30 +1,14 @@
-import { Box, Typography, Switch, Stack } from "@mui/material";
+import { Switch } from "@mui/material";
 
 export default {
   title: "Components/Checkboxes",
 };
 
-export function CheckboxesTemplate(): JSX.Element {
-  return (
-    <Box>
-      <Stack direction="row" alignItems="center" sx={{ m: 2 }}>
-        <Switch defaultChecked />
-        <Typography pl={2}>On/Enabled</Typography>
-      </Stack>
-      <Stack direction="row" alignItems="center" sx={{ m: 2 }}>
-        <Switch />
-        <Typography pl={2}>Off/Enabled</Typography>
-      </Stack>
-      <Stack direction="row" alignItems="center" sx={{ m: 2 }}>
-        <Switch disabled defaultChecked />
-        <Typography pl={2}>On/Inactive</Typography>
-      </Stack>
-      <Stack direction="row" alignItems="center" sx={{ m: 2 }}>
-        <Switch disabled />
-        <Typography pl={2}>Off/Inactive</Typography>
-      </Stack>
-    </Box>
-  );
-}
+export const Checkboxes = ({ disabled, checked }: { disabled: boolean; checked: boolean }) => (
+  <Switch disabled={disabled} defaultChecked={checked} />
+);
 
-CheckboxesTemplate.storyName = "Checkboxes";
+Checkboxes.argTypes = {
+  disabled: { control: { type: "boolean" }, defaultValue: false },
+  checked: { control: { type: "boolean" }, defaultValue: false },
+};
