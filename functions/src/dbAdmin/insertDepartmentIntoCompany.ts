2c2b60c4c5eba880d33829aa3f1acd9a658f20a4
Anton <antonshvetswork@yandex.ru>
2022-06-30 18:58:08 +0300

    delete needless comments

--- functions/src/dbAdmin/insertDepartmentIntoCompany.ts
+++ functions/src/dbAdmin/insertDepartmentIntoCompany.ts
@@ -1,6 +1,5 @@
 import { DepartmentInfo } from "../../../typescript-types/db.types";
 import { db } from "../firebase";
-// import { Chance } from "chance";
 
 interface insertDepartmentIntoCompanyProps {
   domain: string;
@@ -11,9 +10,7 @@ export const insertDepartmentIntoCompany = async ({
   domain,
   departmentInfo,
 }: insertDepartmentIntoCompanyProps): Promise<void> => {
-  // const chance = new Chance();
-  // const uid = chance.hash({ length: 21 });
-
   const companyCollection = db.collection("companies").doc(domain);
+
   await companyCollection.collection("departments").doc().set(departmentInfo);
 };
