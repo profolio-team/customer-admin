a7b50a2aa276083f4285281741388a77d5a9e887
dllsc <92045376+dllsc@users.noreply.github.com>
2022-07-10 19:41:07 +0300

    fix deploy

--- app/src/views/UserInfo/UserInfoPage.tsx
+++ app/src/views/UserInfo/UserInfoPage.tsx
@@ -4,9 +4,15 @@ import { Loader } from "../../components";
 import { useAuth } from "../../hooks/useAuth";
 import db from "../../services/firebase/firestore";
 import { UserPersonalInfoForm } from "./UserPersonalInfoForm";
-
-export function UserInfoPage(): JSX.Element {
-  const { uid, user } = useAuth();
+import { User } from "firebase/auth";
+export function UserInfoPage() {
+  const { user, uid } = useAuth();
+  if (user && uid) {
+    return UserInfoPage1(uid, user);
+  }
+  return <Loader />;
+}
+export function UserInfoPage1(uid: string, user: User): JSX.Element {
   const [userInfoDB] = useCollection(query(db.collections.users, where(documentId(), "==", uid)));
   console.log("userInfo:");
   console.log(userInfoDB);
