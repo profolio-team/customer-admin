9f9dac256cc2379ff46eec95b2e1c0945a790d0e
dllsc <92045376+dllsc@users.noreply.github.com>
2022-07-15 16:23:59 +0300

    update autocomplete for default value

--- app/src/components/Autocompletes/AutocompleteDepartments.tsx
+++ app/src/components/Autocompletes/AutocompleteDepartments.tsx
@@ -1,54 +1,71 @@
 import { Autocomplete, TextField } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCollectionOnce } from "react-firebase-hooks/firestore";
-import { limit, orderBy, query, where } from "firebase/firestore";
+import { documentId, limit, orderBy, query, where } from "firebase/firestore";
 import { Control, Controller } from "react-hook-form";
 import db from "../../services/firebase/firestore";
 import { QueryConstraint } from "@firebase/firestore";
 import { UserInfo } from "../../../../typescript-types/db.types";
 
-export function AutocompleteDepartments({
-  control,
-}: {
-  control: Control<UserInfo>;
-  fieldName: keyof UserInfo;
-}) {
-  const [queryConstraint, setQueryConstraint] = useState<QueryConstraint[]>([]);
-  const [users] = useCollectionOnce(
-    query(db.collections.departments, ...queryConstraint, orderBy("name"), limit(5))
-  );
-  const options = users
-    ? users.docs.map((user) => {
-        return {
-          label: user.data().name,
-          value: user.id,
-        };
-      })
-    : [{ label: "Not find", value: "" }];
+export function AutocompleteDepartments({ control }: { control: Control<UserInfo> }) {
   return (
     <Controller
-      render={({ field: { onChange, value } }) => (
-        <Autocomplete
-          id={"department"}
-          onChange={(event, value) => {
-            onChange(value ? value.value : undefined);
-          }}
-          value={value ? options.find((option) => option.value === value) : null}
-          options={options}
-          renderInput={(params) => (
-            <TextField
-              onChange={(event) => {
+      render={({ field: { onChange, value } }) => {
+        const [queryConstraint, setQueryConstraint] = useState<QueryConstraint[]>(
+          value ? [where(documentId(), "in", [value])] : []
+        );
+        const [users, loading] = useCollectionOnce(
+          query(db.collections.departments, ...queryConstraint)
+        );
+        const [options, setOptions] = useState([{ value: "", label: "Loading.." }]);
+        const [first, setFirst] = useState(true);
+
+        useEffect(() => {
+          if (users) {
+            setOptions(
+              users.docs.map((user) => {
+                return {
+                  label: user.data().name,
+                  value: user.id,
+                };
+              })
+            );
+          }
+        }, [users]);
+        return (
+          <Autocomplete
+            loading={loading}
+            id={"department"}
+            onChange={(event, value) => {
+              onChange(value ? value.value : undefined);
+              if (value) {
+                setFirst(true);
+              }
+            }}
+            value={
+              first
+                ? options.find((p) => p.value === value)
+                  ? options.find((p) => p.value === value)
+                  : null
+                : null
+            }
+            options={options}
+            onInputChange={(event, value: string, reason: string) => {
+              if ((reason === "reset" && !value) || reason === "clear" || reason === "input") {
+                const valueForFind = reason === "input" ? value : "";
+                setFirst(false);
                 setQueryConstraint([
-                  where("name", ">=", event.target.value),
-                  where("name", "<=", `${event.target.value}~`),
+                  where("name", ">=", `${valueForFind}`),
+                  where("name", "<=", `${valueForFind}~`),
+                  orderBy("name"),
+                  limit(5),
                 ]);
-              }}
-              {...params}
-              label="Department"
-            />
-          )}
-        />
-      )}
+              }
+            }}
+            renderInput={(params) => <TextField {...params} label="Department" />}
+          />
+        );
+      }}
       name={"departmentId"}
       control={control}
     />
