f3b374a55665f7f4f05c2d4ca1c082f98403fec6
dllsc <92045376+dllsc@users.noreply.github.com>
2022-07-15 16:23:03 +0300

    add user change corporate info

--- app/src/views/UsersPages/UserRouter.tsx
+++ app/src/views/UsersPages/UserRouter.tsx
@@ -1,12 +1,37 @@
 import { useParams } from "react-router-dom";
-import { UserCompanyInfoForm } from "../../components/AdminUserForm/UserCompanyInfoForm";
 import { invite } from "../../components/UserForm/inviteUser";
 import { ErrorPage } from "../../components/ErrorPage/ErrorPage";
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
+import { Loader } from "../../components";
+import db from "../../services/firebase/firestore";
+import { doc } from "firebase/firestore";
+import { changeUserCorporateInfo } from "../../utils/requests/updateUserCorporateInfo";
+import { UserInfo } from "../../../../typescript-types/db.types";
+import { UserCorporateInfoForm } from "../../components/Forms/UserCorporateInfoForm";
 
 export function UserRouter() {
   const { param } = useParams();
   if (param === "invite") {
-    return <UserCompanyInfoForm postUserInfo={invite} pageTitle={"User Invite"} />;
+    return <UserCorporateInfoForm postUserInfo={invite} pageTitle={"User Invite"} />;
+  }
+  if (typeof param === "string") {
+    const [user, loadingUser] = useDocumentOnce(doc(db.collections.users, param));
+    if (loadingUser) {
+      return <Loader />;
+    }
+    if (!user) {
+      return <ErrorPage code={"404"} />;
+    }
+    const post = (data: UserInfo) => {
+      return changeUserCorporateInfo({ data, uid: user.id });
+    };
+    return (
+      <UserCorporateInfoForm
+        postUserInfo={post}
+        pageTitle={"User Invite"}
+        defaultValues={user.data()}
+      />
+    );
   }
   return <ErrorPage code={"404"} />;
 }
